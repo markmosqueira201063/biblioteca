cc2232b64ebc9da306a130dfb8e4cd47ecdff830
biblioteca <201063@unamba.edu.pe>
2022-09-27 09:35:47 -0500

    Se agrega el Proyecto

---
+++ Config/Config.php
@@ -0,0 +1,9 @@
+<?php
+    const BASE_URL = "http://localhost:8080/biblioteca/";
+    const HOST = "localhost";
+    const BD = "biblioteca";
+    const DB_USER = "root";
+    const PASS = "";
+    const CHARSET = "charset=utf8";
+?>
+
